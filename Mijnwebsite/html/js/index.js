7dbdf99103f8212a59122e92c032350ef4f60262
Mikail <mikail.gurbuz@student.kdg.be>
2021-10-27 16:32:41 +0200

    Oefening

---
+++ Mijnwebsite/html/js/index.js
@@ -0,0 +1,24 @@
+//(function() {
+//    
+//    
+//                                                                     
+//                                                                     
+//                                                                     
+//        function hidePic(){
+//        
+//        
+//   
+//        alert("Hello World!");
+//        document.getElementById("portfolio-image").style.visibility= "hidden";
+//    }
+//
+// document.getElementsByClassName("enlarge-icon").addEventListener("mousedown",hidePic());
+//   
+//    
+//})();
+
+ 
+    
+
+
+
